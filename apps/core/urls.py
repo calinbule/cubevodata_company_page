eed093a7436be9444935a9d7abdbfed8a0b980ad
Calin Bule <admin@easylink.studio>
2025-08-21 18:53:37 +0300

    update

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('microservers/', views.microservers, name='microservers'),
     path('cookies/', views.cookies, name='cookies'),
     path('privacy/', views.privacy, name='privacy'),
     path('terms/', views.terms, name='terms'),
-    path('microservers/', views.microservers, name='microservers'),
 ]
