c80aeb181030a026a0197a54b12a3542128b29d3
Calin Bule <admin@easylink.studio>
2025-08-21 18:12:30 +0300

    updated static root

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # Add this line to use 'static' in project
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
